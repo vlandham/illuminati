c8569c6ffa317de80e54edba92cb949b37760d2a
Jim V <jfv@stowers.org>
2011-07-13 16:54:08 -0500

    error and send email if config test does not pass

--- bin/align_runner.rb
+++ bin/align_runner.rb
@@ -2,55 +2,108 @@
 
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
+BASE_BIN_DIR = File.expand_path(File.dirname(__FILE__))
+POSTRUN_SCRIPT = File.join(BASE_BIN_DIR, "post_runner.rb")
+
 require 'illuminati'
 
-flowcell_id = ARGV[0]
-@test = false
+module Illuminati
+  class AlignRunner
+    attr_accessor :test
 
-output_filename = "run_align_step.out"
+    def initialize
+      @test = false
+      output_filename = "run_align_step.out"
+      @output_file = File.new(output_filename, 'w')
+    end
 
-@output_file = File.new(output_filename, 'w')
+    def finish
+      output "Done"
+      @output_file.close
+    end
 
-def output message
-  @output_file << message << "\n"
-  puts message
-end
+    def output message
+      @output_file << message << "\n"
+      puts message
+    end
 
-def execute command
-  output command
-  system(command) unless @test
-end
+    def execute command
+      output command
+      result = %x[#{command}] unless @test
+      result
+    end
 
-output "starting alignment step for #{flowcell_id}"
-Illuminati::Emailer.email "starting align step for #{flowcell_id}" unless @test
-Illuminati::SolexaLogger::log flowcell_id, "starting alignment", @test
+    def valid_status status_string
+      status_lines = status_string.split("\n")
+      status_lines.each {|line| output "# #{line}\n"}
+      status_lines.each do |line|
+        if line =~ /ERROR/
+          puts "Error in status"
+          return false
+        end
+      end
+      true
+    end
 
-flowcell = nil
+    def run(flowcell_id)
+      output "starting alignment step for #{flowcell_id}"
+      Emailer.email "starting align step for #{flowcell_id}" unless @test
+      SolexaLogger::log flowcell_id, "starting alignment", @test
 
-begin
-  flowcell = Illuminati::FlowcellData.new(flowcell_id)
-rescue Exception => err
-  output "Problem creating flowcell"
-  output "Flowcell id: #{flowcell_id}."
-  output err
-end
+      flowcell = nil
 
-if flowcell
-  config_file = File.join(flowcell.base_dir, "config.txt")
-  if File.exists? config_file
-    command = "#{CASAVA_PATH}/configureAlignment.pl #{config_file}"
-    execute command
-    command += " --make"
-    execute command
+      begin
+        flowcell = FlowcellData.new(flowcell_id)
+      rescue Exception => err
+        output "Problem creating flowcell"
+        output "Flowcell id: #{flowcell_id}."
+        Emailer.email "Error flowcell in aligner for #{flowcell_id}" unless @test
+        output err
+      end
 
-    command = "cd #{flowcell.aligned_dir};"
-    command += " nohup make -j 8 all > make.aligned.out 2>&1  &"
-    execute command
+      if flowcell
+        config_file = File.join(flowcell.base_dir, "config.txt")
+        if File.exists? config_file
+          command = "#{CASAVA_PATH}/configureAlignment.pl #{config_file} 2>&1"
+          status = execute command
 
-  else
-    output "ERROR: no config.txt file found in #{flowcell.base_dir}"
+          if !valid_status(status)
+            output "Problem with config file. Exiting"
+            Emailer.email "Error config.txt file is invalid for #{flowcell_id}" unless @test
+            raise "invalid config file"
+          else
+            output "Config test passed"
+          end
+
+          command += " --make"
+          execute command
+
+          post_command = "#{POSTRUN_SCRIPT} #{flowcell.flowcell_id} > post_run.out 2>&1"
+          command = "cd #{flowcell.aligned_dir};"
+          command += " nohup make -j 8 POST_RUN_COMMAND=\"#{post_command}\" all > make.aligned.out 2>&1  &"
+          execute command
+
+        else
+          output "ERROR: no config.txt file found in #{flowcell.base_dir}"
+        end
+      end
+
+      finish
+    end
   end
 end
 
-output "Done"
-@output_file.close
+if __FILE__ == $0
+  flowcell_id = ARGV[0]
+
+  if flowcell_id
+    puts "Flowcell ID: #{flowcell_id}"
+  else
+    puts "ERROR: call with flowcell id"
+    puts "       align_runner.rb [FLOWCELL_ID]"
+    exit
+  end
+
+  runner = Illuminati::AlignRunner.new
+  runner.run(flowcell_id)
+end
