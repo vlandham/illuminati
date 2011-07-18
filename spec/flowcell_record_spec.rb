0f84af5dd8169339017f254bf52e381d56c3bccc
Jim V <jfv@stowers.org>
2011-07-18 13:06:02 -0500

    spec for custom barcoded samplesheet

--- spec/flowcell_record_spec.rb
+++ spec/flowcell_record_spec.rb
@@ -15,52 +15,73 @@ class FakePaths
 end
 
 describe Illuminati::FlowcellRecord do
-  before(:each) do
-    @valid_id = "639P5AAXX"
-    @paths = FakePaths.new
-    @valid_record = Illuminati::FlowcellRecord.find @valid_id, @paths
+  describe "illumina indexed flowcell" do
+    before(:each) do
+      @valid_id = "639P5AAXX"
+      @paths = FakePaths.new
+      @valid_record = Illuminati::FlowcellRecord.find @valid_id, @paths
+    end
+
+    it "should have an id" do
+      @valid_record.id.should == @valid_id
+    end
+
+    it "should have an lanes" do
+      @valid_record.lanes.size.should == 8
+      @valid_record.lanes[7].samples.size.should == 1
+    end
+
+    it "should output sample sheet data" do
+      sample_sheet_filename = File.join(@paths.base_dir, "#{@valid_id}_SampleSheet.csv")
+      File.exists?(sample_sheet_filename).should == true
+      sample_sheet_example = File.open(sample_sheet_filename, 'r').read
+      @valid_record.to_sample_sheet.should == sample_sheet_example
+    end
+
+    it "should convert to hash" do
+      hash = @valid_record.to_h
+      hash[:samples].size.should == 38
+    end
+
+    it "should not allow modifying of values through hash" do
+      hash = @valid_record.to_h
+      @valid_record.id.should == hash[:flowcell_id]
+      hash[:flowcell_id] = "123"
+      @valid_record.id.should_not == hash[:flowcell_id]
+      @valid_record.lanes[0].samples[0].name.should == hash[:samples][0][:name]
+      hash[:samples][0][:name] = "bad name"
+      @valid_record.lanes[0].samples[0].name.should_not == hash[:samples][0][:name]
+    end
+
+    it "should convert to yaml" do
+      yaml = @valid_record.to_yaml
+      yaml.should_not == nil
+    end
+
+    it "should output config file" do
+      config_filename = File.join(@paths.base_dir, "#{@valid_id}_config.txt")
+      File.exists?(config_filename).should == true
+      config_example = File.open(config_filename, 'r').read
+      @valid_record.to_config_file.should == config_example
+    end
   end
 
-  it "should have an id" do
-    @valid_record.id.should == @valid_id
-  end
+  describe "custom barcoded" do
+    before(:each) do
+      @valid_id = "639P5AAXX"
+      @paths = FakePaths.new
+      @paths.base_dir = File.join(@paths.base_dir, "custom")
+      @valid_record = Illuminati::FlowcellRecord.find @valid_id, @paths
+    end
 
-  it "should have an lanes" do
-    @valid_record.lanes.size.should == 8
-    @valid_record.lanes[7].samples.size.should == 1
-  end
+    it "should output sample sheet data" do
+      sample_sheet_filename = File.join(@paths.base_dir, "#{@valid_id}_SampleSheet.csv")
+      File.exists?(sample_sheet_filename).should == true
+      sample_sheet_example = File.open(sample_sheet_filename, 'r').read
+      @valid_record.to_sample_sheet.should == sample_sheet_example
+    end
 
-  it "should output sample sheet data" do
-    sample_sheet_filename = File.join(@paths.base_dir, "#{@valid_id}_SampleSheet.csv")
-    File.exists?(sample_sheet_filename).should == true
-    sample_sheet_example = File.open(sample_sheet_filename, 'r').read
-    @valid_record.to_sample_sheet.should == sample_sheet_example
-  end
-
-  it "should convert to hash" do
-    hash = @valid_record.to_h
-    hash[:samples].size.should == 38
-  end
-
-  it "should not allow modifying of values through hash" do
-    hash = @valid_record.to_h
-    @valid_record.id.should == hash[:flowcell_id]
-    hash[:flowcell_id] = "123"
-    @valid_record.id.should_not == hash[:flowcell_id]
-    @valid_record.lanes[0].samples[0].name.should == hash[:samples][0][:name]
-    hash[:samples][0][:name] = "bad name"
-    @valid_record.lanes[0].samples[0].name.should_not == hash[:samples][0][:name]
-  end
-
-  it "should convert to yaml" do
-    yaml = @valid_record.to_yaml
-    yaml.should_not == nil
-  end
-
-  it "should output config file" do
-    config_filename = File.join(@paths.base_dir, "#{@valid_id}_config.txt")
-    File.exists?(config_filename).should == true
-    config_example = File.open(config_filename, 'r').read
-    @valid_record.to_config_file.should == config_example
+    it "should " do
+    end
   end
 end
