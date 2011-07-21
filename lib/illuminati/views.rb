ae308d42d89a8a40b003da507f02abd73c33f560
Jim V <jfv@stowers.org>
2011-07-21 12:37:19 -0500

    added documentation. removed unused sample_report view

--- lib/illuminati/views.rb
+++ lib/illuminati/views.rb
@@ -1,19 +1,55 @@
 require 'erb'
 
 module Illuminati
+  #
+  # Responsible for organizing flowcell record into output used to generate a config.txt file for
+  # CASAVA 1.8 ELANDv2e alignment.
+  #
+  # Uses config.txt.erb in assests directory for most of the formating of the config.txt file.
+  #
   class ConfigFileView
+    # Location of the erb template file to use to build config.txt
     CONFIG_TEMPLATE_PATH = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "assests", "config.txt.erb"))
     attr_accessor :lanes, :input_dir, :flowcell_id
+
+    #
+    # Create new instance of view.
+    #
+    # == Parameters
+    # flowcell_record::
+    #   Instance of FlowcellRecord to create a config.txt file from.
     def initialize flowcell_record
       @lanes = squash_lanes(flowcell_record)
       @input_dir = flowcell_record.paths.unaligned_dir
     end
 
+    #
+    # Outputs flowcell record into config.txt format.
+    #
+    # == Returns:
+    # Doesn't actually write to file, but instead returns string representation of config.txt.
+    # This was done mostly as a convience during testing.
+    #
     def write
       template = ERB.new File.new(CONFIG_TEMPLATE_PATH).read, nil, "%<>"
       output = template.result(binding)
     end
 
+    #
+    # Groups similar lanes of flowcell record to make config.txt output shorter.
+    # We try to determine which lanes have the same parameters and then group them
+    # for outputting to config.txt. This step isn't required, but results in config.txt files
+    # that are much shorter and more concise.
+    #
+    # == Parameters:
+    # flowcell_record::
+    #   FlowcellRecord to look at.
+    #
+    # == Returns:
+    # Array of lane data arrays. Each lane data array will have the values needed to
+    # generate config.txt contents for one or more lanes. See the config.txt.erb file
+    # for details on how these are used.
+    #
     def squash_lanes flowcell_record
       squashed_lane_data = []
       current_lane_index = 0
@@ -34,12 +70,32 @@ module Illuminati
 end
 
 module Illuminati
+  #
+  # SampleSheet.csv is required by CASAVA 1.8 to perform demultiplexing and alignment.
+  # This view is responsible for generating this csv file content from a FlowcellRecord.
+  #
   class SampleSheetView
+    #
+    # Create new instance of this view.
+    #
+    # == Parameters:
+    # flowcell_record::
+    #   the FlowcellRecord to generate a SampleSheet.csv from.
     def initialize flowcell_record
       @flowcell = flowcell_record
 
     end
 
+    #
+    # Returns FlowcellRecord data in SampleSheet.csv form. Does not
+    # actually write the contents to file.
+    #
+    # == Returns:
+    # String which can be saved as SampleSheet.csv and contains all the
+    # data required for all lanes / samples. This data is acquired from
+    # the FlowcellRecord which in turn acquires it from the LIMS system and
+    # the SampleMultiplex.csv file (if present).
+    #
     def write
       sample_sheet =  ["fcid", "lane", "sampleid",
                        "sampleref", "index", "description",
@@ -64,31 +120,6 @@ module Illuminati
         end
       end
       sample_sheet
-    end
-  end
-end
-
-module Illuminati
-  class SampleReportView
-    def initialize flowcell_record
-      @flowcell = flowcell_record
-    end
-
-    def write
-      sample_report = ["output", "lane", "sample name", "illumina index", "custom barcode", "read", "reference"].join(",")
-      sample_report += "\n"
-      @flowcell.each_sample_with_lane do |sample, lane|
-        outputs = sample.outputs
-        reads = sample.reads
-        outputs.each_with_index do |output, index|
-        data = []
-        data << output << sample.lane << sample.name << sample.illumina_barcode
-        data << sample.custom_barcode << reads[index] << sample.genome
-        sample_report += data.join(",")
-        sample_report += "\n"
-        end
-      end
-      sample_report
     end
   end
 end
