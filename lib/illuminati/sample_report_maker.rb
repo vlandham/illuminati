76f0418d758fc951e554312df9a1f86265fe8708
Jim V <jfv@stowers.org>
2011-08-24 10:53:30 -0500

    more terribleness with the summary report

--- lib/illuminati/sample_report_maker.rb
+++ lib/illuminati/sample_report_maker.rb
@@ -9,6 +9,7 @@ module Illuminati
   #
   class SampleReportMaker
     DATA_NAMES = [:output, :lane, :name, :illumina, :custom, :read, :genome]
+    attr_reader :demultiplex_filename, :sample_summary_filename
 
     #
     # Makes the SampleReport string. Not actually outputing it to file.
@@ -22,11 +23,8 @@ module Illuminati
     #
     def self.make flowcell
       @flowcell = flowcell
-      demultiplex_filename = File.join(@flowcell.paths.unaligned_stats_dir, "Demultiplex_Stats.htm")
-      sample_report_filename = File.join(@flowcell.paths.aligned_stats_dir, "Sample_Summary.htm")
-      html_parser = HtmlParser.new
-      @demultiplex_data = html_parser.table_data(demultiplex_filename)[0]
-      @sample_report_data = html_parser.table_data(sample_report_filename)
+      @demultiplex_filename = File.join(@flowcell.paths.unaligned_stats_dir, "Demultiplex_Stats.htm")
+      @sample_summary_filename = File.join(@flowcell.paths.aligned_stats_dir, "Sample_Summary.htm")
       @tab_parser = TabFileParser.new
 
       sample_report = ["output", "lane", "sample name", "illumina index",
@@ -55,21 +53,28 @@ module Illuminati
     def self.data_for sample
       all_read_data = []
       sample_datas = sample.sample_report_data
-      demultiplex_data = []
 
       sample_datas.each do |sample_data|
         data = DATA_NAMES.collect {|key| sample_data[key]}
+        demultiplex_data = []
+        sample_summary_data = []
         demultiplex_data = data_from_custom_barcode sample
         if demultiplex_data.empty?
           demultiplex_data = data_from_demultiplex_sample_data sample
+          sample_summary_data = data_from_sample_summary_data sample, sample_data[:read]
+        else
+          # custom barcode doesn't have other info
+          sample_summary_data << "-1" << "-1" << "-1" << "-1"
         end
 
-        data << demultiplex_data
+        data << demultiplex_data unless demultiplex_data.empty?
+        data << sample_summary_data unless sample_summary_data.empty?
         data.flatten!
         all_read_data << data
       end
       all_read_data
     end
+
 
     #
     # Returns the number of reads count from the Demultiplex_Stats.htm
@@ -78,9 +83,8 @@ module Illuminati
     # Return value is a string. nil is returned if count cannot be found.
     #
     def self.data_from_demultiplex_sample_data sample
-      count = nil
-      demultiplex_sample_data = demultiplex_data_for_sample sample, @demultiplex_data
       data = []
+      demultiplex_sample_data = demultiplex_data_for_sample sample
       if !demultiplex_sample_data
         puts "ERROR: sample report maker cannot find demultiplex data for #{sample.id}"
       else
@@ -97,26 +101,30 @@ module Illuminati
       data
     end
 
+    def self.data_from_sample_summary_data sample, read
+      data = []
+      sample_summary_data = sample_summary_data_for_sample sample, read
+      if !sample_summary_data
+        puts "ERROR: sample report maker cannot find sample summary data for #{sample.id}"
+      else
+        data << sample_summary_data["Species"]
+        percent_align = sample_summary_data["% Align (PF)"]
+
+      end
+      data
+    end
+
     #
     # Returns count value from custom barcode output for a particular sample.
     # Return value is a string. nil is returned if count cannot be found or
     # if no custom barcodde output file is present.
     #
     def self.data_from_custom_barcode sample
-      barcode_filename = @flowcell.paths.custom_barcode_path_out(sample.lane.to_i)
       data = []
-      if File.exists? barcode_filename
-        count = nil
-        all_barcode_data = @tab_parser.parse(barcode_filename)
-        barcode_data = barcode_data_for_sample sample, all_barcode_data
-        if !barcode_data
-          puts "ERROR: sample report maker cannot find barcode data for #{sample.id} #{sample.barcode}"
-        else
-          count = barcode_data["Count"]
-          data << count
-          # custom barcode doesn't have other info
-          data << 0 << 0
-        end
+      barcode_data = barcode_data_for_sample sample
+      if barcode_data
+        count = barcode_data["Count"]
+        data << count << "-1" << "-1"
       end
       data
     end
@@ -125,10 +133,48 @@ module Illuminati
     # Returns the barcode data for a particular sample from
     # all possible barcode data. Used by custom barcode count finder.
     #
-    def self.barcode_data_for_sample sample, barcoded_data
-      barcoded_data.each do |barcode_line|
-        if barcode_line["Barcode"] == sample.custom_barcode
-          return barcode_line
+    def self.barcode_data_for_sample sample
+      barcode_filename = @flowcell.paths.custom_barcode_path_out(sample.lane.to_i)
+      if File.exists? barcode_filename
+        all_barcode_data = @tab_parser.parse(barcode_filename)
+        barcoded_data.each do |barcode_line|
+          if barcode_line["Barcode"] == sample.custom_barcode
+            return barcode_line
+          end
+        end
+      end
+      nil
+    end
+
+    #
+    #
+    def self.sample_summary_data_for_sample sample, read
+      if File.exists?(sample_summary_filename)
+        html_parser = HtmlParser.new
+        sample_summary_data = html_parser.table_data(sample_summary_filename)
+        # we need both the barcode-lane summary data and
+        # the sample results summary for the appropriate read.
+        sample_data = []
+        # barcode lane data is in table[0]
+        sample_summary_data[0].each do |barcode_lane_data|
+          if barcode_lane_data["Lane"] == sample.lane.to_s and
+          barcode_lane_data["Barcode"] == sample.barcode_string
+            sample_data << barcode_lane_data
+            break
+          end
+        end
+
+        read_index = read.to_i == 1 ? 1 : 2
+        sample_summary_data[read_index].each do |sample_results_data|
+          if sample_results_data["Sample"] == sample.id
+            sample_data << sample_results_data
+            break
+          end
+        end
+
+        sample_data.flatten!
+        unless sample_data.empty?
+          return sample_data
         end
       end
       nil
@@ -138,14 +184,19 @@ module Illuminati
     # Returns data from Demultiplex_Stats.htm for a particular sample.
     # Sample matching is done using the Sample ID.
     #
-    def self.demultiplex_data_for_sample sample, demultiplex_data
-      demultiplex_data.each do |demultiplex_sample|
-        if demultiplex_sample["Sample ID"] == sample.id
-          return demultiplex_sample
+    def self.demultiplex_data_for_sample sample
+      if File.exists?(demultiplex_filename)
+        html_parser = HtmlParser.new
+        demultiplex_data = html_parser.table_data(demultiplex_filename)[0]
+        demultiplex_data.each do |demultiplex_sample|
+          if demultiplex_sample["Sample ID"] == sample.id
+            return demultiplex_sample
+          end
         end
       end
       nil
     end
+
   end
 end
 
