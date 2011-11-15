2e152ca8fb80886443f24bc499cf3412df5c2c49
Jim V <jfv@stowers.org>
2011-11-15 09:25:30 -0600

    fix outsource post runner with new options

--- bin/outsource_post_runner.rb
+++ bin/outsource_post_runner.rb
@@ -20,10 +20,21 @@ end
 
 if __FILE__ == $0
   flowcell_id = ARGV[0]
+  options = {}
+  opts = OptionParser.new do |o|
+    o.banner = "Usage: post_runner.rb [Flowcell Id] [options]"
+    o.on('-t', '--test', 'do not write out to disk') {|b| options[:test] = b}
+    o.on('-s', "--steps step1,step2,step3" , Array, 'Specify only which steps of the pipeline should be executed') {|b| options[:steps] = b.collect {|step| step} }
+    o.on('-y', '--yaml YAML_FILE', String, "Yaml configuration file that can be used to load options.","Command line options will trump yaml options") {|b| options.merge!(Hash[YAML::load(open(b)).map {|k,v| [k.to_sym, v]}]) }
+    o.on('-h', '--help', 'Displays help screen, then exits') {puts o; exit}
+  end
+
+  opts.parse!
+
   if flowcell_id
     paths = Illuminati::FlowcellPaths.new flowcell_id, OUTSOURCE_TEST, Illuminati::OutsourcePaths
     flowcell = Illuminati::FlowcellRecord.find flowcell_id, paths
-    runner = Illuminati::PostRunner.new flowcell, OUTSOURCE_TEST
+    runner = Illuminati::PostRunner.new flowcell, options
     runner.run
   else
     puts "ERROR: call with flowcell id"
