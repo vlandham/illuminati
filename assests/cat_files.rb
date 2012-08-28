c95abe9edc3336a292b6d0be241288ad842e74e9
Jim V <jfv@stowers.org>
2012-08-28 14:54:13 -0500

    more debugging output

--- assests/cat_files.rb
+++ assests/cat_files.rb
@@ -4,6 +4,9 @@ require 'json'
 database_filename = ARGV[0]
 database_index = ENV["SGE_TASK_ID"].to_i - 1
 
+puts "database file:#{database_filename}"
+puts "index: #{database_index}"
+
 database = JSON.parse(File.open(database_filename, 'r').read)
 
 entry = database[database_index]
